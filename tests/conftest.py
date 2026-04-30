eaaa772b43f3352e0380b73151e4e0cac0feca49
bbulson <b.bulson@outlook.com>
2026-04-30 00:19:16 -0400

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,4 +7,4 @@ def client():
     """
     Provides an instance of the AIClient pointing to the local test server.
     """
-    return AIClient("http://localhost:5000")
+    return AIClient("https://localhost:5000")
